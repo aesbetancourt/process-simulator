56762cded08568aa1331844d0b43d1c266709c29
Alejandro <aesbetancourt@outlook.com>
2019-09-20 01:33:26 -0400

    improved index view

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ let mainWindow;
 function createWindow () {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
+        width: 1010,
+        height: 600,
         webPreferences: {
             nodeIntegration: true
         }
